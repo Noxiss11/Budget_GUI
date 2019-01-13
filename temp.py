2baa0df86c0d1846f578f737b3e73b8152a58c9f
Noxiss1 <noxiss11@gmail.com>
2019-01-13 23:28:15 +0100

    added the ViewPage with treeView and stated working on refreshing values

--- temp.py
+++ temp.py
@@ -1,24 +1,13 @@
-class A(object):
-	"""docstring for A"""
-	def __init__(self):
-		super(A, self).__init__()
-		self.a = 5
-
-class B(A):
-	"""docstring for B"""
-	def __init__(self):
-		super(B, self).__init__()
-		self.b = 8
-		
-
-
-obj = A()
-obj2 = B()
-print(obj2.a,obj2.b)
-	
-class ClassName(object):
-	"""docstring for ClassName"""
-	def __init__(self, arg):
-		super(ClassName, self).__init__()
-		self.arg = arg
-		
+from tkinter import* from tkinter 
+import ttk import sqlite3 
+class Product: db_name = 'database.db' 
+def __init__(self,wind): 
+	self.wind=wind self.wind.title('IT products') 
+	frame = LabelFrame(self.wind, text='Add new record') 
+	frame.grid(row=0, column=1) 
+	Label(frame, text='Name:').grid(row=1, column=1) 
+	self.name = Entry(frame) self.name.grid(row=1, column =2) 
+	Label(frame, text = 'Price:').grid(row=2, column=1) 
+	self.price=Entry(frame) self.price.grid(row=2, column=2) 
+	ttk.Button(frame, text='Add record').grid (row=3,column=2) 
+	self.message=Label(text='', fg='red') self.message.grid(row=3, column=0) self.tree = ttk.Treeview (height=10, columns=2) self.tree.grid(row=4, column=0, columnspan=2) self.tree.heading('#0',text = 'Name', anchor=W) self.tree.heading(2, text='Price', anchor=W) ttk.Button(text= 'Delete record').grid (row=5, column=0) ttk.Button(text= 'Edit record').grid(row=5, column=1) self.viewing_records() def run_query(self, query, parameters =()): with sqlite3.connect (self.db_name) as conn: cursor = conn.cursor() query_result=cursor.execute(query, parameters) conn.commit() return query_result() def viewing_records(self): records = self.tree.get_children() for element in records: self.tree.delete(element) query= 'SELECT * FROM product ORDER BY name DESC' db_rows=self.run_query(query) for row in db_rows: self.tree.insert('',0, text = row[1], values = row[2]) if _name_ == '__main__': wind=Tk() application = Product(wind) wind.mainloop()﻿
--- tk_test.py
+++ tk_test.py
@@ -15,22 +15,30 @@ class DateFunctions:
 	def DateFormat(number):
 
 		
-	    if int(number) < 10:
-	        return "0" + str(number)
-	    return number
+		if int(number) < 10:
+			return "0" + str(number)
+		return number
 
-	
-	CurrentYearMonth = str(datetime.datetime.date(datetime.datetime.now()).year) + '-' + str(
-	datetime.datetime.date(datetime.datetime.now()).month)
+	if (int(datetime.datetime.date(datetime.datetime.now()).month)<10):
+
+		CurrentYearMonth = str(datetime.datetime.date(datetime.datetime.now()).year) + '-0' + str(
+		datetime.datetime.date(datetime.datetime.now()).month)
+	else:
+		CurrentYearMonth = str(datetime.datetime.date(datetime.datetime.now()).year) + '-' + str(
+		datetime.datetime.date(datetime.datetime.now()).month)
+
 
 	CurrentDate = str(datetime.datetime.date(datetime.datetime.now()).year) + '-' + DateFormat(str(
 	datetime.datetime.date(datetime.datetime.now()).month)) + '-' + DateFormat(str(
-	    datetime.datetime.date(datetime.datetime.now()).day))
+		datetime.datetime.date(datetime.datetime.now()).day))
 
 #main class for for frame taht will be always open and in which we will see all the pages
 class Window(Tk):
 
 	def __init__(self):
+		global DB
+		DB=Database()
+
 		#creates container for all pages
 		Tk.__init__(self)
 		container = Frame(self)
@@ -49,7 +57,7 @@ class Window(Tk):
 		#creates dictionary for list of all pages
 		self.frames = {}
 		#creates the pages from the dictionary
-		for F in (StartPage, MainPage, AddPage):
+		for F in (StartPage, MainPage, AddPage, ViewPage):
 			frame = F(container, self)
 			self.frames[F] = frame
 			frame.grid(row=0, column=0,sticky='nsew')
@@ -57,18 +65,22 @@ class Window(Tk):
 		self.show_frame(StartPage)
 
 	def show_frame(self,cont):
-		
+	
 		frame = self.frames[cont]
+		frame.update_idletasks()
 		frame.tkraise()
+		
 
 class MenuBar(Menu):
 	
 	def __init__(self,parent):
 		Menu.__init__(self,parent)
+
+	
 		
 		self.add_command(label='Main', command = lambda: app.show_frame(MainPage))
 		self.add_command(label='Add', command = lambda: app.show_frame(AddPage))
-		self.add_command(label='View', command = lambda: app.show_frame(MainPage))
+		self.add_command(label='View', command = lambda: app.show_frame(ViewPage))
 		self.add_command(label='Savings', command = lambda: app.show_frame(MainPage))
 		self.add_command(label='Forecast', command = lambda: app.show_frame(MainPage))
 		self.add_command(label='Instruments', command = lambda: app.show_frame(MainPage))
@@ -93,40 +105,97 @@ class MainPage(Frame):
 	def __init__(self,parent,controller):
 			Frame .__init__(self,parent)
 
+			
+
 			names = ['eKonto','Cash','eMax','Retirement','Target','Emergency','Fun']
 			Przychody = ['Wynagrodzenie', 'Przychody z kapitalu', 'Inne przychody']
 			Rozchody = ['Zywnosc', 'Transport', 'Odziez', 'Telefon', 'Higiena', 'Zdrowie', 'Edukacja', 'Rekreacja', 'Uzywki',
-            'Dary', 'Inne', 'Oplaty', 'Srodki trwale', 'Mieszkanie', 'Oplaty mieszkanie', 'Wspolne']
+			'Dary', 'Inne', 'Oplaty', 'Srodki trwale', 'Mieszkanie', 'Oplaty mieszkanie', 'Wspolne']
 
-
-
+			global RozchodyVariables, PrzychodyVariables, namesVariables,MonthEntry2
+			RozchodyVariables = {}
+			PrzychodyVariables = {}
+			namesVariables = {}
+			
+			YearMonth=DateFunctions.CurrentYearMonth
 			MonthLabel = Label(self,text='Month')
 			MonthLabel.grid(row=0,column=0)
 
 			MonthEntryVariable = StringVar()
 			MonthEntryVariable .set(DateFunctions.CurrentYearMonth)
+			
 
 			MonthEntry = Entry(self,textvariable = MonthEntryVariable)
 			MonthEntry.grid(row=0,column=1)
-			
+			MonthEntry2 = MonthEntry
 
 			###creates the labels for accounts
 			i=2
 			for name in names:
 				LabelName = Label(self,text=name)
 				LabelName.grid(row=i, column=0)
+
+				Value=StringVar()
+				Value.set(DB.GetValue(name))
+
+				LabelValue = Label(self,textvariable=Value)
+				LabelValue.grid(row=i,column=1)
+
 				i=i+1
+			#############
+			##PRZYCHODY##
+			#############	
 			i=i+2
 			for name in Przychody:
 				LabelName = Label(self,text=name)
 				LabelName.grid(row=i, column=0)
+
+				#PrzychodyVariables[name] = DB.GetAmount(name,MonthEntryVariable.get())
+				PrzychodyVariables[name]=StringVar()
+				PrzychodyVariables[name].set(DB.GetAmount(name,MonthEntryVariable.get()))
+
+				LabelValue = Label(self,textvariable = PrzychodyVariables[name])
+				LabelValue.grid(row=i,column=1)
+
 				i=i+1
 
+			############		
+			##ROZCHODY##
+			############
 			i=i-len(Przychody)
 			for name in Rozchody:
 				LabelName = Label(self,text=name)
 				LabelName.grid(row=i, column=3)
+
+
+				RozchodyVariables[i] = DB.GetAmount(name,MonthEntryVariable.get())
+				Value=StringVar()
+				Value.set(RozchodyVariables[i])
+
+				LabelValue = Label(self,textvariable=Value)
+				LabelValue.grid(row=i,column=4)
+
+
+
 				i=i+1
+
+
+				RefreshButton = Button(self,text = 'Refresh', command = lambda: DB.Refresh())
+				RefreshButton.grid(row = 0, column =2)
+				global ValueTest
+				ValueTest=StringVar()
+				ValueTest.set(DB.GetAmount('Wynagrodzenie',MonthEntryVariable.get()))
+
+				LabelValueTest= Label(self,textvariable=ValueTest)
+				LabelValueTest.grid(row=7,column=7)
+
+
+
+		
+
+			
+
+
 
 class AddPage(Frame):
 
@@ -137,54 +206,56 @@ class AddPage(Frame):
 
 			Przychody = ['Wynagrodzenie', 'Przychody z kapitalu', 'Inne przychody']
 			Rozchody = ['Zywnosc', 'Transport', 'Odziez', 'Telefon', 'Higiena', 'Zdrowie', 'Edukacja', 'Rekreacja', 'Uzywki',
-            'Dary', 'Inne', 'Oplaty', 'Srodki trwale', 'Mieszkanie', 'Oplaty mieszkanie', 'Wspolne']
+			'Dary', 'Inne', 'Oplaty', 'Srodki trwale', 'Mieszkanie', 'Oplaty mieszkanie', 'Wspolne']
 			Transfer = ['Cash-->eKonto', 'eKonto-->eMax plus-Emerytura', 'eKonto-->eMax plus-Cel dlugo-dystansowy',
-            'eKonto-->eMax-Emergency',
-            'eKonto-->eMax-Fundusz rozrywkowy', 'eKonto-->Cash', 'eKonto-->Obligacje-Emerytura',
-            'eKonto-->Obligacje-Cel dlugo-dystansowy',
-            'eMax plus- Emerytura-->eKonto', 'eMax plus-Cel dlugo-dystansowy-->eKonto', 'eMax-Emergency-->eKonto',
-            'eMax-Fundusz rozrywkowy-->eKonto',
-            'Skarbonka -->Gotówka', 'Obligacje-Emerytura-->eKonto', 'Obligacje-Cel dlugo-dystansowy-->eKonto']
+			'eKonto-->eMax-Emergency',
+			'eKonto-->eMax-Fundusz rozrywkowy', 'eKonto-->Cash', 'eKonto-->Obligacje-Emerytura',
+			'eKonto-->Obligacje-Cel dlugo-dystansowy',
+			'eMax plus- Emerytura-->eKonto', 'eMax plus-Cel dlugo-dystansowy-->eKonto', 'eMax-Emergency-->eKonto',
+			'eMax-Fundusz rozrywkowy-->eKonto',
+			'Skarbonka -->Gotówka', 'Obligacje-Emerytura-->eKonto', 'Obligacje-Cel dlugo-dystansowy-->eKonto']
 		
 			Types = ('Przychody', 'Rozchody', 'Transfer')
 			Payments = (
 			'eKonto', 'Cash', 'eMax plus-Emerytura', 'eMax plus-Cel dlugo-dystansowy', 'eMax-Emergency', 'eMax-Fundusz rozrywkowy')
 			Amounts = []
+			
+
 			global Type, Category, OptionCategory, OptionPayment
 			def set_options(*args):
-			    
+				
 
-			    if Type.get() == '(default)':
-			        return None
+				if Type.get() == '(default)':
+					return None
 
-			    # refresh Category
-			    Category.set('(select)')
-			    OptionCategory['menu'].delete(0, 'end')
+				# refresh Category
+				Category.set('(select)')
+				OptionCategory['menu'].delete(0, 'end')
 
-			    # pick new set of options
+				# pick new set of options
 
-			    if Type.get() == 'Przychody':
-			        new_options = Przychody
-			        print('przychody')
-			    elif Type.get() == 'Rozchody':
-			        new_options = Rozchody
-			    else:
-			        new_options = []
+				if Type.get() == 'Przychody':
+					new_options = Przychody
+					print('przychody')
+				elif Type.get() == 'Rozchody':
+					new_options = Rozchody
+				else:
+					new_options = []
 
-			    # add new options in
-			    for item in new_options:
-			        OptionCategory['menu'].add_command(label=item, command=tk._setit(Category, item))
+				# add new options in
+				for item in new_options:
+					OptionCategory['menu'].add_command(label=item, command=tk._setit(Category, item))
 
-			    Payment.set('eKonto')
-			    OptionPayment['menu'].delete(0, 'end')
+				Payment.set('eKonto')
+				OptionPayment['menu'].delete(0, 'end')
 
-			    if Type.get() == 'Transfer':
-			        new_options2 = Transfer
-			    else:
-			        new_options2 = Payments
+				if Type.get() == 'Transfer':
+					new_options2 = Transfer
+				else:
+					new_options2 = Payments
 
-			    for item in new_options2:
-			        OptionPayment['menu'].add_command(label=item, command=tk._setit(Payment, item))
+				for item in new_options2:
+					OptionPayment['menu'].add_command(label=item, command=tk._setit(Payment, item))
 
 
 			Labels=['Date','Type','Category','Amount','Payment']
@@ -230,15 +301,37 @@ class AddPage(Frame):
 
 			OptionPayment = OptionMenu(self, Payment, *Payments)
 			OptionPayment.grid(row=4, column=1)
-			DB=Database()
+			
 			AddButton = Button(self,text='Add', command=lambda: DB.Add())
 			AddButton.grid(row=5, column=1)
+
+
+class ViewPage(Frame):
+
+	def __init__(self,parent,controller):
+			Frame .__init__(self,parent)
+
+			tree = ttk.Treeview(self,columns = ('ID','Date','Type','Category','Amount','Payment'))
+			tree.grid(row = 0, column = 0, columnspan=6)
+
+			records = tree.get_children()
+			db_rows = DB.c.execute("SELECT ID,Date,Type,Category, Amount, Payment from dane")
+			for row in db_rows:
+				print(row)
+				tree.insert('',0,text=row[0], values=(row[1], row[2],row[3],row[4],row[5]))
 
 class Database:
 
 	db = sql.connect('Budget.db')
 	c = db.cursor()
 
+
+	def Refresh(self):
+		print(MonthEntry2)
+		for a in PrzychodyVariables:
+			PrzychodyVariables[a].set(self.GetAmount(a,str(MonthEntry2.get())[0:7]))
+
+		print('refreshed')
 	def CreateTable(self):
 
 		self.c.execute('''CREATE TABLE IF NOT EXISTS dane(ID INTEGER PRIMARY KEY, Date DATE,
@@ -254,13 +347,38 @@ class Database:
 		self.c.execute('INSERT INTO dane(Date, Type, Category, Amount, Payment) VALUES(?,?,?,?,?)',
 			(str(EntryDateVariable.get()), str(Type.get()), str(Category.get()), str(EntryAmount.get()), str(Payment.get())))
 		self.db.commit()
-    	
+		self.Refresh()
+	
+	def GetAmount(self,CategoryName,YearMonth):
+		self.YearMonth=YearMonth
+		print(self.YearMonth, CategoryName)
+		self.c.execute('SELECT SUM(Amount) FROM dane where SUBSTR(Date,1,7) = ? AND Category =?',[str(YearMonth), str(CategoryName)])
+		ValueTemp = self.c.fetchall()
+		Value = [Value1[0] for Value1 in ValueTemp]
+		length = len(str(Value)) - 1
+
+		# print(str(Value)=='[None]')
+		if str(Value) == '[None]':
+			return 0.00
+		else:
+			return round(float(str(Value)[1:length]), 2)
 			
+	def GetValue(self,CategoryName):
+		self.c.execute('SELECT SUM(Amount) FROM dane where Payment =?', [str(CategoryName)])
+		ValueTemp = self.c.fetchall()
+		Value = [Value1[0] for Value1 in ValueTemp]
+		length = len(str(Value)) - 1
 
-DB = Database()
-DB.CreateTable()
+		# print(str(Value)=='[None]')
+		if str(Value) == '[None]':
+			return 0.00
+		else:
+			return round(float(str(Value)[1:length]), 2)
 
-	       
+	
+		
+
+		   
 app = Window()
 app.geometry('800x600')
 app.title('Budget')
